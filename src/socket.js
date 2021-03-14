b72a832b39e8c3163b488a2cbcb868c98446496d
nguyentrancu <nguyentrancu97@gmail.com>
2021-03-14 21:33:09 +0700

    update

---
+++ src/socket.js
@@ -0,0 +1,20 @@
+let app = require('express')();
+let http = require('http').Server(app);
+console.log(http);
+let io = require('socket.io')(http, {
+    origin: "https://example.com", methods: ["GET", "POST"], allowEIO3: true
+});
+let Redis = require('ioredis');
+let redis = new Redis();
+
+redis.subscribe('chat-channel', function (err, count) {
+});
+
+redis.on('message', function (channel, message) {
+    message = JSON.parse(message);
+    io.emit(channel + ':' + message.event, message.data);
+});
+
+http.listen(9000, function () {
+    console.log('Listening on Port 9000');
+});
